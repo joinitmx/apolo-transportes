b15855db403adbb63dde280377f22eb2c0adbce4
joinitmx <44792674+joinitmx@users.noreply.github.com>
2018-11-08 22:10:47 -0600

    Update ConsultaEntradasSalidas.vb

--- apolo transportes/apolo transportes/Models/ConsultaEntradasSalidas.vb
+++ apolo transportes/apolo transportes/Models/ConsultaEntradasSalidas.vb
@@ -19,11 +19,11 @@ Public Class ConsultaEntradasSalidasListResponse
         End Set
     End Property
 
-    Public Property Entradas As String
+    Public Property Entradas As Integer
         Get
             Return vEntradas
         End Get
-        Set(value As String)
+        Set(value As Integer)
             vEntradas = value
         End Set
     End Property
@@ -37,6 +37,61 @@ Public Class ConsultaEntradasSalidasListResponse
         End Set
     End Property
 
+    Public Property Fecha As String
+        Get
+            Return vFecha
+        End Get
+        Set(value As Fecha)
+            vFecha = value
+        End Set
+    End Property
+    
+    Public Property idOrigenes As Integer
+        Get
+            Return vidOrigenes
+        End Get
+        Set(value As Integer)
+            vidOrigenes = value
+        End Set
+    End Property
+    
+    Public Property idChofer As Integer
+        Get
+            Return vidChofer
+        End Get
+        Set(value As Integer)
+            vidChofer = value
+        End Set
+    End Property
+    
+    Public Property idVehiculo As Integer
+        Get
+            Return vidVehiculo
+        End Get
+        Set(value As Integer)
+            vidVehiculo = value
+        End Set
+    End Property
+    
+    Public Property idTipoCarga As Integer
+        Get
+            Return vidTipoCarga
+        End Get
+        Set(value As Integer)
+            vidTipoCarga = value
+        End Set
+    End Property
+    
+    Public Property idTipoTransporte As Integer
+        Get
+            Return vidTipoTransporte
+        End Get
+        Set(value As Integer)
+            vidTipoTransporte = value
+        End Set
+    End Property
+    
+    
 End Class
 
 Public Class ConsultaEntradasSalidasListRequest
