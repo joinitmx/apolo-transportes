034dbd82abee6d3078a086927b32033120b2bb3b
joinitmx <44792674+joinitmx@users.noreply.github.com>
2018-11-08 20:44:54 -0600

    Create ConsultaEntradasSalidas.vb
    Recuperación de datos de entradas y salidas de vehiculos

---
+++ apolo transportes/apolo transportes/Models/ConsultaEntradasSalidas.vb
@@ -0,0 +1,56 @@
+Public Class ConsultaEntradasSalidas
+    Private vEntradas As Integer
+    Private vSalidas As String
+    Private vId As Integer
+    Private vFecha As Date
+    Private vidChofer As Integer
+    Private vidVehiculo As Integer
+    Private vidChofer As Integer
+    Private vidOrigenes As Integer
+    Private vidTipoCarga As Integer
+    Private vidTipoTransporte As Integer
+    Private vEstatus As Integer
+    
+    Public Property Id As Integer
+        Get
+            Return vId
+        End Get
+        Set(ByVal value As Integer)
+            vId = value
+        End Set
+    End Property
+
+    Public Property Entradas As String
+        Get
+            Return vEntradas
+        End Get
+        Set(value As String)
+            vEntradas = value
+        End Set
+    End Property
+
+    Public Property Salidas As Integer
+        Get
+            Return vSalidas
+        End Get
+        Set(value As Integer)
+            vSalidas = value
+        End Set
+    End Property
+
+End Class
+
+Public Class ConsultaEntradasSalidasListRequest
+
+    Private vTipo As Integer
+
+    Public Property Tipo As Integer
+        Get
+            Return vTipo
+        End Get
+        Set(ByVal value As Integer)
+            vTipo = value
+        End Set
+    End Property
+
+End Class
